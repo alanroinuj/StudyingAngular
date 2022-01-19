645575de8dcc699d8e52bccd63a79ac5d6d05766
Alan Junior <alanxizde@gmail.com>
2022-01-19 17:13:24 -0300

    aplicando js e ajuste do css

--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'sidebar-component',
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
- btn = document.querySelector('#btn');
-  constructor() { }
+
+  //adicionando classe CSS na tag da DOM  e ao Template com ViewChild
+  @ViewChild('isActive') isActive!:ElementRef;
+
+  constructor() {
+
+  }
+
+  openMenu(){
+    this.isActive.nativeElement.classList.toggle("isActive");
+  }
 
   ngOnInit(): void {
+
   }
 
 }
